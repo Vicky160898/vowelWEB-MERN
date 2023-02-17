faff13cadc994e865126693bbb06764a672b15dc
vinayak hiwase <vinayakhiwse8@gmail.com>
2023-02-17 19:26:09 +0530

    vowelweb

--- view/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createUser, loginUser } = require("../controller/user");
-const router = express.Router();
-
-//user route for register and login..
-router.post("/register", createUser);
-router.post("/login", loginUser);
-
-module.exports = router;
